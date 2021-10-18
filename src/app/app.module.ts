108efdd3781b14e83a0ff6fb3da5d53a47780da5
nknoppik <noah.knoppik@gmx.de>
2021-10-18 13:21:19 +0200

    basic UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,11 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatSortModule } from '@angular/material/sort';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -24,7 +29,12 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     FlexLayoutModule,
     MatToolbarModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSortModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatSort} from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(MatSort) sort:MatSort
+
+  project_table: any
+  displayedColumns: string[]
+  dummy_data = [
+    {name: "Dummy1", description: "Some Dummy dataset", id: "007"},
+    {name: "Dummy2", description: "Second Dummy dataset", id: "2042"}
+  ]
+  constructor() { 
+    this.displayedColumns=["name", "description", "id", "details","generateKeys", "deleteProjects"]
+    this.project_table = new MatTableDataSource(this.dummy_data)
+    
+    console.log(this.project_table)
+  }
 
   ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit():void{
+    this.project_table.sort = this.sort
+  }
+
+  applyFilter(event: Event) {
+    //filter table
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.project_table.filter = filterValue.trim().toLowerCase();
+  }
+  viewKeys(id){
+    console.log("Generate API Key for ", id)
+  }
+  deleteProject(id){
+    console.log("Delete Project", id)
+  }
+  viewDataset(id){
+    console.log("View Project", id)
   }
 
 }
