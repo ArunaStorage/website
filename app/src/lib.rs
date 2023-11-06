8f6feaeaf727b9f01621a854e81877f6cde508f0
Sebastian Beyvers <sb@pus.de>
2023-11-06 23:43:08 +0100

    feat: Tested encrypted cookie jar functionalities for server functions

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -9,18 +9,35 @@ pub mod utils;
 
 #[server(GetUserInfo, "/api", "GetJson")]
 pub async fn get_user_info() -> Result<WhoamiResponse, ServerFnError> {
-    use axum_extra::extract::CookieJar;
+    use axum_core::response::IntoResponse;
+    use axum_extra::extract::cookie::Cookie;
+    use axum_extra::extract::cookie::Key;
+    use axum_extra::extract::PrivateCookieJar;
+    use leptos_axum::ResponseOptions;
+    use leptos_axum::ResponseParts;
     use utils::aruna_api_handlers::ConnectionHandler;
 
     let req_parts = use_context::<leptos_axum::RequestParts>()
         .ok_or_else(|| ServerFnError::Request("Invalid context".to_string()))?;
-    let jar = CookieJar::from_headers(&req_parts.headers);
+
+    let a_key: Key =
+        use_context::<Key>().ok_or(ServerFnError::ServerError("No server state".to_string()))?;
+    let jar = PrivateCookieJar::from_headers(&req_parts.headers, a_key);
 
     match jar.get("logged_in") {
         Some(l) if l.value() == "false" => return Ok(WhoamiResponse::NotLoggedIn),
         None => return Ok(WhoamiResponse::NotLoggedIn),
         _ => {}
     }
+
+    /*     let jar = jar.add(Cookie::new("whatever", "true"));
+
+    let response = expect_context::<ResponseOptions>();
+
+    response.overwrite(ResponseParts {
+        headers: jar.into_response().into_parts().0.headers,
+        ..Default::default()
+    }); */
     if let Some(cookie) = jar.get("token") {
         let token = cookie.value().to_string();
         return Ok(ConnectionHandler::aruna_who_am_i(&token).await);
