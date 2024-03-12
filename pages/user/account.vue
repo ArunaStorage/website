7fb47f18bb5b8866716144664496d65ab6296507
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2024-03-12 18:09:00 +0100

    feat: Migrate parts of the user account page

--- pages/user/account.vue
+++ pages/user/account.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { IconArrowLeft, IconBuildingWarehouse, IconCheck, IconCross, IconLockOpen, IconPlus, IconUser, IconX } from '@tabler/icons-vue';
+import { IconArrowLeft, IconBuildingWarehouse, IconCheck, IconCross, IconLockOpen, IconPlus, IconPokeball, IconUser, IconUserScan, IconX } from '@tabler/icons-vue';
 import type { v2Token } from '~/composables/aruna_api_json';
 
 // Redirect to login if not logged in
@@ -20,6 +20,62 @@ function getTokens(): v2Token[] {
 </script>
 
 <template>
+
+  <div class="flex flex-wrap justify-between container mx-auto mt-10">
+    <h1 class="text-3xl font-bold text-gray-700 dark:text-white">
+      Overview User
+    </h1>
+    <button @click="router.back()"
+      class="cursor-pointer px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:bg-blue-100 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:bg-blue-800/30 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+      <IconArrowLeft class="icon" />
+    </button>
+  </div>
+
+  <div class="md:container sm:mx-1 md:mx-auto mt-4 p-4 border rounded-lg">
+    <div class="border-b border-gray-200 dark:border-gray-700">
+      <nav class="flex space-x-2" aria-label="Tabs" role="tablist">
+        <button type="button"
+          class="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-blue-500 active"
+          id="tabs-with-icons-item-1" data-hs-tab="#tabs-with-icons-1" aria-controls="tabs-with-icons-1" role="tab">
+          <IconUserScan class="flex-shrink-0 size-4" />
+          Profile
+        </button>
+        <button type="button"
+          class="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-blue-500"
+          id="tabs-with-icons-item-2" data-hs-tab="#tabs-with-icons-2" aria-controls="tabs-with-icons-2" role="tab">
+          <IconPokeball class="flex-shrink-0 size-4" />
+          Tokens
+        </button>
+        <button type="button"
+          class="hs-tab-active:font-semibold hs-tab-active:border-blue-600 hs-tab-active:text-blue-600 py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-blue-500"
+          id="tabs-with-icons-item-3" data-hs-tab="#tabs-with-icons-3" aria-controls="tabs-with-icons-3" role="tab">
+          <IconBuildingWarehouse class="flex-shrink-0 size-4" />
+          Data Proxies
+        </button>
+      </nav>
+    </div>
+
+    <div class="mt-3">
+      <div id="tabs-with-icons-1" role="tabpanel" aria-labelledby="tabs-with-icons-item-1">
+        <p class="text-gray-500 dark:text-gray-400">
+          This is the <em class="font-semibold text-gray-800 dark:text-gray-200">first</em> item's tab body.
+        </p>
+      </div>
+      <div id="tabs-with-icons-2" class="hidden" role="tabpanel" aria-labelledby="tabs-with-icons-item-2">
+        <p class="text-gray-500 dark:text-gray-400">
+          This is the <em class="font-semibold text-gray-800 dark:text-gray-200">second</em> item's tab body.
+        </p>
+      </div>
+      <div id="tabs-with-icons-3" class="hidden" role="tabpanel" aria-labelledby="tabs-with-icons-item-3">
+        <p class="text-gray-500 dark:text-gray-400">
+          This is the <em class="font-semibold text-gray-800 dark:text-gray-200">third</em> item's tab body.
+        </p>
+      </div>
+    </div>
+  </div>
+
+
+
   <div class="page-wrapper d-print-none">
     <div class="page-header">
       <div class="container-xl">
@@ -30,9 +86,7 @@ function getTokens(): v2Token[] {
           </div>
           <div class="col-auto ms-auto d-print-none">
             <div class="btn-list">
-              <a @click="router.back()" class="btn btn-ghost-secondary d-none d-sm-inline-block pe-0 ps-3">
-                <IconArrowLeft class="icon" />
-              </a>
+
             </div>
           </div>
         </div>
