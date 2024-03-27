cf200b3bc05633fa8fa48d5fd9aefc1a8053596b
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2024-03-27 18:11:11 +0100

    feat: Restore admin page functionality

--- composables/api_wrapper.ts
+++ composables/api_wrapper.ts
@@ -2,16 +2,16 @@
 import type { v2User } from "./aruna_api_json/models/v2User"
 import { v2DataClass, type v2CreateProjectRequest, type v2ResourceWithPermission, type v2KeyValue, type v2DeactivateUserResponse, type v2ActivateUserResponse, type v2GetAllUsersResponse, type v2Permission, type v2CreateAPITokenResponse, type v2CreateAPITokenRequest } from "./aruna_api_json"
 
-export async function fetchUser(): Promise<v2User | string> {
-    const user = await $fetch('/api/user').catch((e) => {
-        console.log(e)
-        return e as string
+export async function fetchUser(id: string | undefined): Promise<v2User | string> {
+    const user = await $fetch(id ? `/api/user?userId=${id}` : '/api/user').catch((e) => {
+        return e.toString()
     })
     return user
 }
 
 export async function fetchUsers(): Promise<v2User[] | undefined> {
     const users = await $fetch('/api/users')
+    console.log(users)
     return users
 }
 
@@ -23,14 +23,11 @@ export async function activateUser(userId: string): Promise<boolean> {
 }
 
 export async function deactivateUser(userId: string): Promise<boolean> {
-    // Check if token needs refresh
-    await $fetch('/oidc/user')
     // Deactivate user
     const response = await $fetch(`/api/user/${userId}/deactivate`, {
         method: 'PATCH'
     })
-    return true
-}
+    return response !== undefined}
 
 export async function createUserToken(name: string, scope: v2Permission | undefined, expiry: string | undefined): Promise<v2CreateAPITokenResponse | undefined> {
     // Create request and send
@@ -49,40 +46,45 @@ export async function createUserToken(name: string, scope: v2Permission | undefi
 }
 
 
-export async function fetchUserResources(user: v2User): Promise<v2ResourceWithPermission[]> {
-        // Filter projects from user permissions
-        const projectPermissions = user.attributes?.personalPermissions?.filter((perm) => perm.projectId)
-        // Fetch resources
-        if (projectPermissions && projectPermissions.length > 0) {
-            let fetchUrl = '/api/resources?'
-            projectPermissions.forEach((perm, idx, arr) => {
-                if (perm.projectId) {
-                    fetchUrl += arr.length - 1 === idx ? `resourceIds=${perm.projectId}` : `resourceIds=${perm.projectId}&`
-                }
-            })
+export async function fetchUserResources(user: v2User | undefined): Promise<v2ResourceWithPermission[]> {
 
-            const resources = await $fetch<v2ResourceWithPermission[]>(fetchUrl)
-            return resources
-        }
-
+    if (user === undefined) {
         return []
+    }
+
+    // Filter projects from user permissions
+    const projectPermissions = user.attributes?.personalPermissions?.filter((perm) => perm.projectId)
+    // Fetch resources
+    if (projectPermissions && projectPermissions.length > 0) {
+        let fetchUrl = '/api/resources?'
+        projectPermissions.forEach((perm, idx, arr) => {
+            if (perm.projectId) {
+                fetchUrl += arr.length - 1 === idx ? `resourceIds=${perm.projectId}` : `resourceIds=${perm.projectId}&`
+            }
+        })
+
+        const resources = await $fetch<v2ResourceWithPermission[]>(fetchUrl)
+        return resources
+    }
+
+    return []
 }
 
 export async function createProject(name: string, description: string, keyValues: v2KeyValue[], dataClass: v2DataClass) {
-        // Create request and send
-        const request = {
-            name: name,
-            description: description,
-            keyValues: keyValues,
-            relations: [],
-            dataClass: dataClass,
-            preferredEndpoint: '',
-            metadataLicenseTag: '',
-            defaultDataLicenseTag: ''
-        } as v2CreateProjectRequest
+    // Create request and send
+    const request = {
+        name: name,
+        description: description,
+        keyValues: keyValues,
+        relations: [],
+        dataClass: dataClass,
+        preferredEndpoint: '',
+        metadataLicenseTag: '',
+        defaultDataLicenseTag: ''
+    } as v2CreateProjectRequest
 
-        await $fetch('/api/project', {
-            method: 'POST',
-            body: request
-        })
+    await $fetch('/api/project', {
+        method: 'POST',
+        body: request
+    })
 }
---
+++ composables/utils.ts
@@ -0,0 +1,50 @@
+import type { v2CustomAttribute } from "./aruna_api_json/models/v2CustomAttribute";
+import type { v2Permission } from "./aruna_api_json/models/v2Permission";
+import type { v2Token } from "./aruna_api_json/models/v2Token";
+import type { v2User } from "./aruna_api_json/models/v2User";
+
+/* ---------- USER ---------- */
+export function isUserAdmin(user: v2User | undefined): boolean {
+    if (user?.attributes?.globalAdmin) {
+        return user.attributes.globalAdmin
+    }
+    return false
+}
+
+export function isUserServiceAccount(user: v2User | undefined): boolean {
+    if (user?.attributes?.serviceAccount) {
+        return user.attributes.serviceAccount
+    }
+    return false
+}
+
+export function getUserCustomAttributes(user: v2User | undefined): v2CustomAttribute[] {
+    if (user?.attributes?.customAttributes) {
+        return user.attributes.customAttributes
+    }
+    return []
+}
+
+export function getUserEndpoints(user: v2User | undefined): string[] {
+    if (user?.attributes?.trustedEndpoints) {
+        return user.attributes.trustedEndpoints
+    }
+    return []
+}
+
+export function getUserToken(user: v2User | undefined): v2Token[] {
+    if (user?.attributes?.tokens) {
+        return user.attributes.tokens
+    }
+    return []
+}
+
+export function getUserPermissions(user: v2User | undefined): v2Permission[] {
+    if (user?.attributes?.personalPermissions) {
+        return user.attributes.personalPermissions
+    }
+    return []
+}
+
+export function getUserResources() {}
+/* -------------------- */
