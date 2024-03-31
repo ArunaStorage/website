b9e4459805fa1d7fb9711d0d9f6b48e866b711b3
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2024-03-31 23:16:49 +0200

    feat: Update user resources page layout

--- pages/user/resources.vue
+++ pages/user/resources.vue
@@ -16,11 +16,11 @@ const name = ref('')
 const description = ref('')
 
 async function tryCreateProject() {
-    console.log(name.value)
-    console.log(description.value)
+  console.log(name.value)
+  console.log(description.value)
 
-    const project = await createProject(name.value, description.value, [], v2DataClass.DATA_CLASS_PUBLIC)
-    console.log("Successfully created Project: " + project)
+  const project = await createProject(name.value, description.value, [], v2DataClass.DATA_CLASS_PUBLIC, '', '')
+  console.log("Successfully created Project: " + project)
 }
 
 // Used for back link to last page in navigation history
@@ -28,89 +28,37 @@ const router = useRouter()
 </script>
 
 <template>
-    <NavbarTop />
+  <NavbarTop />
 
-    <div class="container-xl text-start mt-3">
-        <div class="row mb-4">
-            <div class="col">
-                <div class="page-pretitle text-start">Personal Permissions</div>
-                <h2 class="page-title">Resources</h2>
-            </div>
-            <div class="col-auto ms-auto d-print-none text-end">
-                <div class="btn-list">
-                    <a @click="router.back()" class="btn btn-ghost-secondary d-none d-sm-inline-block pe-0 ps-3">
-                        <IconArrowLeft class="icon" />
-                    </a>
-                    <NuxtLink to="/objects/create" class="btn btn-primary d-none d-sm-inline-block">
-                        <IconPlus class="icon" />
-                        Create new
-                    </NuxtLink>
-                    <!-- 
-                    <button @click="tryCreateProject" class="btn btn-primary d-none d-sm-inline-block">
-                        <IconPlus class="icon" />
-                        Create new
-                    </button>
-                     -->
-                </div>
-            </div>
+  <div class="flex flex-wrap justify-between container mx-auto my-10">
+    <h1 class="text-3xl font-bold text-gray-700 dark:text-white">
+      Projects of {{ user_state?.displayName }}
+    </h1>
+    <button @click="router.back()"
+      class="cursor-pointer px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-gray-700 hover:bg-gray-300 hover:text-gray-800 dark:text-gray-300 dark:hover:bg-gray-800/30 dark:hover:text-gray-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+      <IconArrowLeft class="icon" />
+    </button>
+  </div>
+
+  <div class="flex md:container sm:mx-1 md:mx-auto mt-4 p-4 border border-gray-500 rounded-lg ">
+    <div class="flex flex-col grow">
+      <div class="flex flex-row grow justify-end">
+        <a href="/objects/create"
+          class="py-1 px-2 mt-2 inline-flex gap-x-2 text-md rounded-lg bg-aruna-800 border border-gray-200 text-slate-100 hover:border-aruna-800 hover:text-gray-400 disabled:opacity-50 disabled:pointer-events-none dark:border-gray-700 dark:text-gray-400 dark:hover:text-blue-500 dark:hover:border-blue-600 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+          Create new resouce
+      </a>
+      </div>
+
+      <div class="flex flex-row grow">
+        <div v-for="resource in resources" class="flex flex-row">
+          <CardResource v-if="resource.resource" :resource="resource.resource" />
         </div>
-        <div class="row mt-2">
-            <div class="col">
-
-                <!-- 
-                <div class="mb-3">
-                    <label class="form-label">Name</label>
-                    <input v-model="name" type="text" class="form-control" name="project-name-input" placeholder="Project name" />
-                </div>
-            </div>
-            <div class="col">
-                <div class="mb-3">
-                    <label class="form-label">Description</label>
-                    <input v-model="description" type="text" class="form-control" name="peroject-description-input"
-                        placeholder="Project description" />
-                </div>
- -->
-
-
-                <Suspense>
-                    <div v-for="resource in resources" class="card mb-2">
-                        <ResourceCard v-if="resource.resource" :resource="resource.resource" />
-                    </div>
-
-                    <!-- 
-                    <div v-for="" class="card mb-2">
-                        {entry.get_card_status()}
-                        <div class="card-body d-flex container flex-column">
-                            {entry.get_ribbon()} <div class="row">
-                                <div class="col-4">
-                                    <div>
-                                        <a class="text-primary" href=absolute_link()>
-                                            <h3>{name}</h3>
-                                        </a>
-                                        <a class="subheader" href=absolute_link()>
-                                            <h4>{id}</h4>
-                                        </a>
-                                    </div>
-                                    {entry.get_status()}
-                                    {entry.get_stats()}
-                                    {res.1.get_visualization()}
-                                </div>
-                                <div class="col border-start me-4 container">
-                                    <div class="border-bottom pb-3 mb-2">{entry.get_key_values()}</div>
-                                    <div class="row">
-                                        <h4 class="subheader mb-0">"Description"</h4>
-                                        <p class="text-secondary mb-0">{entry.description}</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    -->
-
-                </Suspense>
-            </div>
+        <div class="flex flex-col grow" v-if="resources.length == 0">
+          Seems like you do not own any resources.
         </div>
+      </div>
     </div>
-    
-    <Footer />
+  </div>
+
+  <Footer />
 </template>
