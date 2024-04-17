c6eaf4efbe29c32d6961a0bc9f1702fa4e1ce7b8
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2024-04-17 16:28:24 +0200

    fix: Anonymous resource fetch

--- server/api/resource.get.ts
+++ server/api/resource.get.ts
@@ -1,16 +1,14 @@
-
-
-
 import type { v2GetResourceResponse } from '~/composables/aruna_api_json'
 
 export default defineEventHandler(async event => {
+    const access_token = getCookie(event, 'access_token') // Does not matter if undefined
     const resourceId = getQuery(event)['resourceId']   
     const baseUrl = useRuntimeConfig().serverHostUrl
     const fetchUrl = `${baseUrl}/v2/resources/${resourceId}` 
     const response = await $fetch<v2GetResourceResponse>(fetchUrl, {
-        headers: {
-            'Authorization': `Bearer ${event.context.access_token}`
-        }
+        headers: access_token ? {
+            'Authorization': `Bearer ${access_token}`
+        } : {}
     })
 
     return response.resource
--- server/api/resources.get.ts
+++ server/api/resources.get.ts
@@ -1,9 +1,8 @@
-
-import type { v2GetResourcesResponse } from '~/composables/aruna_api_json'
+import type {v2GetResourcesResponse} from '~/composables/aruna_api_json'
 
 export default defineEventHandler(async event => {
+    const access_token = getCookie(event, 'access_token') // Does not matter if undefined
     const resourceIds = getQuery(event)['resourceIds']
-
     const baseUrl = useRuntimeConfig().serverHostUrl
     const fetchUrl = new URL(`${baseUrl}/v2/resources`)
 
@@ -16,9 +15,9 @@ export default defineEventHandler(async event => {
     }
 
     const response = await $fetch<v2GetResourcesResponse>(fetchUrl.toString(), {
-        headers: {
-            'Authorization': `Bearer ${event.context.access_token}`
-        }
+        headers: access_token ? {
+            'Authorization': `Bearer ${access_token}`
+        } : {}
     })
 
     return response.resources
