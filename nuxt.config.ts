264015c216f622d8fb2262dd4daa2c8461c49698
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2024-06-26 21:35:53 +0200

    feat: Extend config with mailing list related properties

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,8 +32,12 @@ export default defineNuxtConfig({
   },
 
   runtimeConfig: {
+    serverHostUrl: "http://localhost:8080",
     logRequests: true,
     serverHostUrl: "http://localhost:8080",
+    mailingListHost: 'https://mailing-lists.example.de',
+    mailingListUrl: 'https://mailing-lists.example.de/lists/aruna/',
+    mailingListSubscribe: 'https://mailing-lists.example.de/lists/aruna/anonymous_subscribe',
     provider: {
       local: {
         clientId: "test",
