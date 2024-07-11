7b83ec28844b60f6075e52aba8a7e21661bfdd0c
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2024-07-11 14:21:32 +0200

    feat: Add news page to top navigation and sidebar

--- components/navigation/top.vue
+++ components/navigation/top.vue
@@ -39,6 +39,12 @@ onMounted(() => {
             Search
           </NuxtLink>
 
+          <NuxtLink
+              class="font-medium text-gray-600 dark:text-gray-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+              to="/news">
+            News
+          </NuxtLink>
+
           <!-- Dropdown Start -->
           <div class="hs-dropdown [--strategy:static] sm:[--strategy:fixed] [--adaptive:none]">
             <button id="hs-mega-menu-basic-dr" type="button"
@@ -48,18 +54,18 @@ onMounted(() => {
             </button>
 
             <div
-              class="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 z-10 bg-white sm:shadow-md rounded-lg p-2 dark:bg-gray-800 sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute top-full sm:border before:-top-5 before:start-0 before:w-full before:h-5 hidden"
+              class="hs-dropdown-menu transition-[opacity,margin] duration-[0.1ms] sm:duration-[150ms] hs-dropdown-open:opacity-100 opacity-0 sm:w-48 z-10 bg-white sm:shadow-md rounded-md p-2 dark:bg-gray-800 sm:dark:border dark:border-gray-700 dark:divide-gray-700 before:absolute top-full sm:border before:-top-5 before:start-0 before:w-full before:h-5 hidden"
               style="">
-              <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-300 dark:hover:bg-gray-70"
+              <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-300 dark:hover:bg-gray-70"
                 target="_blank" href="https://github.com/ArunaStorage/api">
                 API
               </a>
-              <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-300 dark:hover:bg-gray-700"
+              <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-300 dark:hover:bg-gray-700"
                 target="_blank"
                 href="https://arunastorage.github.io/documentation/latest/get_started/basic_usage/00_index/">
                 Getting Started
               </a>
-              <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-300 dark:hover:bg-gray-700"
+              <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-300 dark:hover:bg-gray-700"
                 target="_blank"
                 href="https://arunastorage.github.io/documentation/latest/internal_data_structure/internal_data_structure/">
                 Data Structure
@@ -90,7 +96,7 @@ onMounted(() => {
         <button type="button"
           :key="forceRefresh"
           :class="{'hidden' : !user_state}"
-          class="py-1 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 text-gray-600 shadow-sm disabled:opacity-50 disabled:pointer-events-none dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+          class="py-1 px-2 inline-flex items-center gap-x-2 text-sm font-medium rounded-md border border-gray-200 text-gray-600 shadow-sm disabled:opacity-50 disabled:pointer-events-none dark:border-gray-700 dark:text-white dark:hover:bg-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
           data-hs-overlay="#docs-sidebar" aria-controls="docs-sidebar" aria-label="Toggle navigation">
           <span class="sr-only">Toggle Navigation</span>
           <!-- <IconUserCircle class="mx-1 flex-shrink-0 w-5 h-auto" /> -->
