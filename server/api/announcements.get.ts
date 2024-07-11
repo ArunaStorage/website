56ac440f6f90ff2947cee15805adffc3df0d5673
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2024-07-11 14:22:08 +0200

    chore: Clean up

--- server/api/announcements.get.ts
+++ server/api/announcements.get.ts
@@ -1,13 +1,10 @@
 import type {v2GetAnnouncementsResponse} from '~/composables/aruna_api_json'
 
 export default defineEventHandler(async event => {
-
   const pagination = getQuery(event)
-  console.log(pagination)
-
   const baseUrl = useRuntimeConfig().serverHostUrl
   const fetchUrl = `${baseUrl}/v2/info/announcements`
   return await $fetch<v2GetAnnouncementsResponse>(fetchUrl, {
-    query: pagination
+    query: pagination ? pagination : {}
   })
 })
