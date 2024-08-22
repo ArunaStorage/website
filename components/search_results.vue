ecb09c3223303c931822fcb20b341348f99ccc85
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2024-08-22 09:04:27 +0200

    chore: Improve styling of search 'no results' message

--- components/search_results.vue
+++ components/search_results.vue
@@ -10,6 +10,6 @@ const props = defineProps<{
   <CardResource v-for="hit in resources" :resource="hit" />
 
   <div v-if="resources.length == 0">
-    <h2 class="text-muted">"No results found"</h2>
+    <h2 class="text-slate-500 font-bold text-center">No results found</h2>
   </div>
 </template>
