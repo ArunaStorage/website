a00eeed8da6cd557fd3ee5de4f3cfedc2caec321
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2024-10-16 16:21:43 +0200

    chore: Adapt about page to updated layout

--- pages/about.vue
+++ pages/about.vue
@@ -1,42 +1,47 @@
+<script setup lang="ts">
+import TopNavigation from "~/components/navigation/TopNavigation.vue";
+</script>
 <template>
   <NavigationTop/>
 
-  <div class="md:container sm:mx-1 md:mx-auto mt-4 p-4 border-2 border-gray-400 rounded-md">
-    <h1 class="text-3xl font-bold text-gray-800 dark:text-white">
-      About us
-    </h1>
+  <div class="min-h-[calc(100vh-120px)]">
+    <div class="md:container sm:mx-1 md:mx-auto mt-4 p-4 border-2 border-gray-400 rounded-md bg-[#080d1f]">
+      <h1 class="text-3xl font-bold text-gray-800 dark:text-white">
+        About us
+      </h1>
 
-    <hr class="my-6 border-gray-400">
+      <hr class="my-6 border-gray-400">
 
-    <h2 class="mt-6 mb-2 text-2xl font-bold text-gray-600 dark:text-gray-500">
-      Who are we?
-    </h2>
-    <hr class="border-gray-400">
-    <p class="mt-2 p-4 text-gray-800 dark:text-gray-400">
-      We are a small team of developers from the Justus-Liebig-University in Gießen interested in developing advanced
-      cloud native infrastructure and applications.
-    </p>
+      <h2 class="mt-6 mb-2 text-2xl font-bold text-white">
+        Who are we?
+      </h2>
+      <hr class="border-gray-400">
+      <p class="mt-2 p-4 text-gray-800 dark:text-gray-400">
+        We are a small team of developers from the Justus-Liebig-University in Gießen interested in developing advanced
+        cloud native infrastructure and applications.
+      </p>
 
-    <h2 class="mt-6 mb-2 text-2xl font-bold text-gray-600 dark:text-gray-500">
-      Why?
-    </h2>
-    <hr class="border-gray-400">
-    <p class="mt-2 p-4 text-gray-800 dark:text-gray-400">
-      As one of the major cloud locations for the de.NBI network, we have years of experience in developing and
-      operating cloud-native infrastructures. With this expertise, we aim to further develop the fundamental way in
-      which research data is managed and stored in the future.
-    </p>
+      <h2 class="mt-6 mb-2 text-2xl font-bold text-white">
+        Why?
+      </h2>
+      <hr class="border-gray-400">
+      <p class="mt-2 p-4 text-gray-800 dark:text-gray-400">
+        As one of the major cloud locations for the de.NBI network, we have years of experience in developing and
+        operating cloud-native infrastructures. With this expertise, we aim to further develop the fundamental way in
+        which research data is managed and stored in the future.
+      </p>
 
-    <h2 class="mt-6 mb-2 text-2xl font-bold text-gray-600 dark:text-gray-500">
-      What is our goal?
-    </h2>
-    <hr class="border-gray-400">
-    <p class="mt-2 p-4 text-gray-800 dark:text-gray-400">
-      Aruna Object Storage is a multi-domain project that was first envisioned as the core cloud storage component of
-      the Research Data Commons architecture in NFDI4BioDiversity, and later adopted by the FAIR Data Spaces project and
-      NFDI4Microbiota. This multi-domain expertise has resulted in a flexible and generalizable approach to the base
-      storage layer that can be adapted to any data management solution in any domain.
-    </p>
+      <h2 class="mt-6 mb-2 text-2xl font-bold text-white">
+        What is our goal?
+      </h2>
+      <hr class="border-gray-400">
+      <p class="mt-2 p-4 text-gray-800 dark:text-gray-400">
+        Aruna is a multi-domain project that was first envisioned as the core cloud storage component of
+        the Research Data Commons architecture in NFDI4BioDiversity and later adopted by the FAIR Data Spaces project
+        and NFDI4Microbiota. This multi-domain expertise has resulted in a flexible and generalizable approach to the
+        base data orchestration layer that can be adapted to any data management solution in any domain.
+      </p>
+    </div>
   </div>
 
   <Footer/>
