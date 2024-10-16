73a0dc0eec8a21a157ece89e87dd4e1cd6afbe94
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2024-10-16 17:10:51 +0200

    refactor: Adapt user account page to updated layout

--- pages/user/account.vue
+++ pages/user/account.vue
@@ -214,6 +214,7 @@ async function getS3Credentials(endpoint: v2Endpoint) {
         })
       })
 }
+
 /* ----- END CREDENTIALS DIALOG ----- */
 </script>
 
@@ -233,148 +234,142 @@ async function getS3Credentials(endpoint: v2Endpoint) {
                :with-button="false"
                @update:open="setVisibility(Dialogs.TokenDialog, false)"/>
 
-  <div
-      class="md:container sm:mx-1 md:mx-auto mt-4 p-4 border-2 border-gray-400 rounded-md  dark:bg-white/[.75]">
-    <div class="border-b border-gray-400 dark:border-gray-700">
-      <nav class="flex space-x-6" aria-label="Tabs" role="tablist">
-        <button type="button"
-                class="hs-tab-active:font-semibold hs-tab-active:border-aruna-800 hs-tab-active:text-aruna-800 py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-lg whitespace-nowrap text-gray-500 hover:text-aruna-800 focus:outline-none focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-blue-500 active"
-                id="tabs-with-icons-item-1" data-hs-tab="#tabs-with-icons-1" aria-controls="tabs-with-icons-1"
-                role="tab">
-          <IconUserScan class="flex-shrink-0"/>
-          Profile
-        </button>
-        <button type="button"
-                class="hs-tab-active:font-semibold hs-tab-active:border-aruna-800 hs-tab-active:text-aruna-800 py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-lg whitespace-nowrap text-gray-500 hover:text-aruna-800 focus:outline-none focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-blue-500"
-                id="tabs-with-icons-item-2" data-hs-tab="#tabs-with-icons-2" aria-controls="tabs-with-icons-2"
-                role="tab">
-          <IconPokeball class="flex-shrink-0 size-4"/>
-          Tokens
-        </button>
-        <button type="button"
-                class="hs-tab-active:font-semibold hs-tab-active:border-aruna-800 hs-tab-active:text-aruna-800 py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-lg whitespace-nowrap text-gray-500 hover:text-aruna-800 focus:outline-none focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-blue-500"
-                id="tabs-with-icons-item-3" data-hs-tab="#tabs-with-icons-3" aria-controls="tabs-with-icons-3"
-                role="tab">
-          <IconBuildingWarehouse class="flex-shrink-0 size-4"/>
-          Data Proxies
-        </button>
-      </nav>
-    </div>
-
-    <div class="mt-3">
-      <div id="tabs-with-icons-1" role="tabpanel" aria-labelledby="tabs-with-icons-item-1">
-        <div
-            class="bg-yellow-100 border border-yellow-200 text-sm text-yellow-800 rounded-lg p-4 dark:bg-yellow-800/10 dark:border-yellow-900 dark:text-yellow-500"
-            role="alert" v-if="!is_active()">
-          <span class="font-bold">Info: </span>
-          Your account is currently not active. We will activate your account as soon as possible.
-        </div>
-        <div class="flex flex-auto gap-4">
-
-          <div
-              class="flex flex-auto flex-col bg-transparent text-center dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-            <h3 class="text-lg font-bold text-gray-600 dark:text-white">
-              ID
-            </h3>
-            <p class="mt-2 text-gray-500 dark:text-gray-400">
-              {{ get_user()?.id }}
-            </p>
-          </div>
-
-          <div
-              class="flex flex-auto flex-col text-center dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-            <h3 class="text-lg font-bold text-gray-600 dark:text-white">
-              Display Name
-            </h3>
-            <p class="mt-2 text-gray-500 dark:text-gray-400">
-              {{ get_user()?.displayName }}
-            </p>
-          </div>
-
-          <div class="flex flex-auto flex-col text-center  dark:border-gray-700 dark:shadow-slate-700/[.7]">
-            <h3 class="text-lg font-bold text-gray-800 dark:text-white">
-              Email
-            </h3>
-            <p class="mt-2 text-gray-600 dark:text-gray-400">
-              {{ get_user()?.email ? get_user()?.email : "No email provided" }}
-            </p>
-          </div>
-
-          <div
-              class="flex flex-auto flex-col text-center dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]">
-            <h3 class="text-lg font-bold text-gray-600 dark:text-white">
-              Is active:
-            </h3>
-            <p class="flex grow-0 items-center justify-center mt-2 text-gray-500 dark:text-gray-400">
-              <IconCheck v-if="get_user()?.active" class="mx-2 flex-shrink-0 text-green-600"/>
-              <IconX v-else class="icon text-red"/>
-            </p>
-          </div>
-        </div>
+  <div class="min-h-[calc(100vh-120px)]">
+    <div class="md:container sm:mx-1 md:mx-auto mt-[10vh] p-4 border-2 border-gray-400 rounded-md bg-[#080d1f]">
+      <div class="border-b border-gray-400 dark:border-gray-700">
+        <nav class="flex space-x-6" aria-label="Tabs" role="tablist">
+          <button type="button"
+                  class="hs-tab-active:font-semibold hs-tab-active:border-aruna-800 hs-tab-active:text-aruna-700 py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-lg whitespace-nowrap text-gray-500 hover:text-aruna-800 focus:outline-none focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-blue-500 active"
+                  id="tabs-with-icons-item-1" data-hs-tab="#tabs-with-icons-1" aria-controls="tabs-with-icons-1"
+                  role="tab">
+            <IconUserScan class="flex-shrink-0"/>
+            Profile
+          </button>
+          <button type="button"
+                  class="hs-tab-active:font-semibold hs-tab-active:border-aruna-800 hs-tab-active:text-aruna-700 py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-lg whitespace-nowrap text-gray-500 hover:text-aruna-800 focus:outline-none focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-blue-500"
+                  id="tabs-with-icons-item-2" data-hs-tab="#tabs-with-icons-2" aria-controls="tabs-with-icons-2"
+                  role="tab">
+            <IconPokeball class="flex-shrink-0 size-4"/>
+            Tokens
+          </button>
+          <button type="button"
+                  class="hs-tab-active:font-semibold hs-tab-active:border-aruna-800 hs-tab-active:text-aruna-700 py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-lg whitespace-nowrap text-gray-500 hover:text-aruna-800 focus:outline-none focus:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-blue-500"
+                  id="tabs-with-icons-item-3" data-hs-tab="#tabs-with-icons-3" aria-controls="tabs-with-icons-3"
+                  role="tab">
+            <IconBuildingWarehouse class="flex-shrink-0 size-4"/>
+            Data Proxies
+          </button>
+        </nav>
       </div>
 
-      <div id="tabs-with-icons-2" class="hidden" role="tabpanel" aria-labelledby="tabs-with-icons-item-2">
-        <div class="flex flex-col">
-          <div class="-m-1.5 overflow-x-auto">
-            <div class="p-1.5 min-w-full inline-block align-middle">
-              <div class="overflow-hidden">
-                <table class="min-w-full divide-y divide-gray-400 dark:divide-gray-700">
-                  <thead>
-                  <tr>
-                    <th scope="col" class="px-6 py-3 text-start text-md font-medium text-gray-500 uppercase">ID</th>
-                    <th scope="col" class="px-6 py-3 text-start text-md font-medium text-gray-500 uppercase">Name</th>
-                    <th scope="col" class="px-6 py-3 text-start text-md font-medium text-gray-500 uppercase">Last Used
-                    </th>
-                    <th scope="col" class="px-6 py-3 text-start text-md font-medium text-gray-500 uppercase">Expiry date
-                    </th>
-                    <th scope="col" class="px-6 py-3 text-end text-md font-medium text-gray-500 uppercase">Actions
-                    </th>
-                  </tr>
-                  </thead>
+      <div class="mt-3">
+        <div id="tabs-with-icons-1" role="tabpanel" aria-labelledby="tabs-with-icons-item-1">
+          <div v-if="!is_active()"
+              class="my-6 text-center bg-yellow-100 border border-yellow-200 text-sm text-yellow-800 rounded-lg p-4 dark:bg-yellow-800/10 dark:border-yellow-900 dark:text-yellow-500"
+              role="alert" >
+            <span class="font-bold">
+              Info:</span> Your account is currently not active. We will activate your account as soon as possible.
+          </div>
 
-                  <tbody v-if="getTokens().length > 0" class="divide-y divide-gray-400 dark:divide-gray-700">
-                  <tr v-for="token in getTokens()" class="hover:bg-gray-100 dark:hover:bg-gray-700">
-                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
-                      {{ token.id }}
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                      {{ token.name }}
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                      Not yet implemented
-                    </td>
-                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                      {{ token.expiresAt ? df.format(new Date(token.expiresAt)) : 'Expiry date is undefined' }}
-                    </td>
+          <div class="flex flex-col gap-y-4 lg:flex-row justify-between">
+            <Card class="basis-1/5">
+              <CardHeader class="text-center">
+                <CardTitle class="text-xl">User Id</CardTitle>
+                <CardDescription class="text-lg">{{ get_user()?.id || '-' }}</CardDescription>
+              </CardHeader>
+            </Card>
 
-                    <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
-                      <button v-if="token.id" type="button"
-                              @click="deleteToken(token.id)"
-                              class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border p-1 me-2 border-slate-300 text-gray-700 hover:text-red-600 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-                        <IconTrash/>
-                      </button>
-                    </td>
-                  </tr>
-                  </tbody>
-                  <tbody v-else>
-                  <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
-                    <td colspan="4" class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
-                      <strong>Looks like you currently have no active tokens!</strong>
-                    </td>
-                  </tr>
-                  </tbody>
-                </table>
+            <Card class="basis-1/5">
+              <CardHeader class="text-center">
+                <CardTitle class="text-xl">Display Name</CardTitle>
+                <CardDescription class="text-lg">{{ get_user()?.displayName || '-' }}</CardDescription>
+              </CardHeader>
+            </Card>
+
+            <Card class="basis-1/5">
+              <CardHeader class="text-center">
+                <CardTitle class="text-xl">Email</CardTitle>
+                <CardDescription class="text-lg">{{ get_user()?.email ? get_user()?.email : "No email provided" }}
+                </CardDescription>
+              </CardHeader>
+            </Card>
+
+            <Card class="basis-1/5">
+              <CardHeader class="text-center">
+                <CardTitle class="text-xl">Active</CardTitle>
+                <CardDescription class="flex text-lg items-center justify-center">
+                  <IconCheck v-if="is_active()" class="flex-shrink-0 size-8 text-green-600"/>
+                  <IconX v-else class="flex-shrink-0 text-red-600"/>
+                </CardDescription>
+              </CardHeader>
+            </Card>
+          </div>
+        </div>
+
+        <div id="tabs-with-icons-2" class="hidden" role="tabpanel" aria-labelledby="tabs-with-icons-item-2">
+          <div class="flex flex-col">
+            <div class="-m-1.5 overflow-x-auto">
+              <div class="p-1.5 min-w-full inline-block align-middle">
+                <div class="overflow-hidden">
+                  <table class="min-w-full divide-y divide-gray-400 dark:divide-gray-700">
+                    <thead>
+                    <tr>
+                      <th scope="col" class="px-6 py-3 text-start text-md font-medium text-gray-500 uppercase">ID</th>
+                      <th scope="col" class="px-6 py-3 text-start text-md font-medium text-gray-500 uppercase">Name</th>
+                      <th scope="col" class="px-6 py-3 text-start text-md font-medium text-gray-500 uppercase">Last Used
+                      </th>
+                      <th scope="col" class="px-6 py-3 text-start text-md font-medium text-gray-500 uppercase">Expiry
+                        date
+                      </th>
+                      <th scope="col" class="px-6 py-3 text-end text-md font-medium text-gray-500 uppercase">
+                        Actions
+                      </th>
+                    </tr>
+                    </thead>
+
+                    <tbody v-if="getTokens().length > 0" class="divide-y divide-gray-400 dark:divide-gray-700">
+                    <tr v-for="token in getTokens()" class="hover:bg-gray-100 dark:hover:bg-gray-700">
+                      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-gray-200">
+                        {{ token.id }}
+                      </td>
+                      <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
+                        {{ token.name }}
+                      </td>
+                      <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
+                        Not yet implemented
+                      </td>
+                      <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
+                        {{ token.expiresAt ? df.format(new Date(token.expiresAt)) : 'Expiry date is undefined' }}
+                      </td>
+
+                      <td class="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                        <button v-if="token.id" type="button"
+                                @click="deleteToken(token.id)"
+                                class="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border p-1 me-2 border-slate-300 text-gray-700 hover:text-red-600 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+                          <IconTrash/>
+                        </button>
+                      </td>
+                    </tr>
+                    </tbody>
+                    <tbody v-else>
+                    <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
+                      <td colspan="4" class="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">
+                        <strong>Looks like you currently have no active tokens!</strong>
+                      </td>
+                    </tr>
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           </div>
+          <div class="flex flex-row justify-end">
+            <Button @click="setVisibility(Dialogs.TokenDialog, true)"
+                    class="mt-2 bg-aruna-800 hover:bg-aruna-700 text-white text-md rounded-sm">
+              Create Token
+            </Button>
+          </div>
         </div>
-        <div class="flex flex-row justify-end">
-          <Button @click="setVisibility(Dialogs.TokenDialog, true)"
-                  class="mt-2 bg-aruna-800 hover:bg-aruna-700 text-white text-md rounded-sm">
-            Create Token
-          </Button>
-        </div>
-      </div>
 
         <div id="tabs-with-icons-3" class="hidden" role="tabpanel" aria-labelledby="tabs-with-icons-item-3">
           <div class="flex flex-auto flex-wrap gap-x-4 gap-y-4 text-gray-600">
@@ -423,6 +418,7 @@ async function getS3Credentials(endpoint: v2Endpoint) {
         </div>
       </div>
     </div>
+  </div>
 
   <Footer/>
 </template>
