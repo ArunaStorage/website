38d20a4dad923523371828f75b3295a45e80ab9e
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2024-10-16 15:51:26 +0200

    feat: Reintegrate support partners

---
+++ components/custom-ui/frontpage/Supporters.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- Brands -->
+  <div class="mt-6 lg:mt-12">
+    <div
+        class="pt-3 translate-y-2.5 flex items-center text-md text-gray-800 before:flex-[1_1_0%] before:border-t before:border-gray-400 before:me-6 after:flex-[1_1_0%] after:border-t after:border-gray-400 after:ms-6 dark:text-white dark:before:border-gray-600 dark:after:border-gray-600">
+      Supported by
+    </div>
+
+    <div class="py-4 bg-white/[.1] flex gap-x-8 justify-center items-center">
+      <div class="shrink">
+        <img alt="Logo of the FAIR Data Spaces project"
+             src="../../../assets/imgs/fair-ds-logo.png"
+             class="w-40 object-bottom object-contain h-auto"/>
+      </div>
+      <div class="shrink">
+        <img alt="Logo of the NFDI4Microbiota project"
+             src="../../../assets/imgs/microbiota.png"
+             class="w-24 object-bottom object-contain h-auto"/>
+      </div>
+      <div class="shrink">
+        <img alt="Logo of the NFDI4Biodiversity project"
+             src="../../../assets/imgs/biodiv.png"
+             class="w-40 object-bottom object-contain h-auto"/>
+      </div>
+      <div class="shrink">
+        <img alt="Logo of the de.NBI project"
+             src="../../../assets/imgs/denbi.svg"
+             class="w-28 object-bottom object-contain h-auto"/>
+      </div>
+    </div>
+  </div>
+  <!-- End Brands -->
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,21 @@
+<script setup lang="ts">
+import Supporters from "~/components/custom-ui/frontpage/Supporters.vue";
+</script>
 <template>
-  <div class="flex flex-col flex-grow md:h-auto">
+  <div class="flex flex-col flex-grow">
     <!-- Header + Navigation -->
     <NavigationTop/>
+    <!-- END Header + Navigation -->
+
+    <!-- Frontpage Sections -->
     <SectionHero/>
     <SectionFeatures/>
     <SectionFeatures2/>
     <SectionNewsletter/>
     <SectionFaq/>
+    <Supporters/>
+    <!-- END Frontpage Sections -->
+
     <Footer/>
   </div>
 </template>
