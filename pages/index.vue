80f524aaea18ff59af08229eb07f04aa4b018097
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2024-10-16 16:06:15 +0200

    chore: Move hero section file

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@ import FeaturesAruna from "~/components/custom-ui/frontpage/FeaturesAruna.vue";
 import FeaturesGeneral from "~/components/custom-ui/frontpage/FeaturesGeneral.vue";
 import Newsletter from "~/components/custom-ui/frontpage/Newsletter.vue";
 import Faq from "~/components/custom-ui/frontpage/Faq.vue";
+import Hero from "~/components/custom-ui/frontpage/Hero.vue";
 </script>
 <template>
   <div class="flex flex-col flex-grow">
@@ -12,7 +13,7 @@ import Faq from "~/components/custom-ui/frontpage/Faq.vue";
     <!-- END Header + Navigation -->
 
     <!-- Frontpage Sections -->
-    <SectionHero/>
+    <Hero/>
     <FeaturesAruna/>
     <FeaturesGeneral/>
     <Newsletter/>
